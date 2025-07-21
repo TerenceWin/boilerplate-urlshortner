362c10011b8dcc27063eb64224474fc17349f737
Terence Win <terencewin@Terences-MacBook-Air.local>
2025-07-21 17:25:58 +0900

    Saving progress 1

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -21,4 +23,23 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+let urlCounter = 1; 
+let urlArray = {}; 
+
+app.post('/api/shorturl', (req, res) => {
+  const originURL = req.body.url; // assuming form input name="url"
+  const shortURL = urlCounter++; 
+
+  if (!/^https?:\/\/[a-z]+/.test(originURL)) {
+    return res.status(400).json({ error: "Invalid URL" });
+  }else{
+      res.json({
+        original_url: originURL,
+        short_url: shortURL
+      });
+  }
+
+  
 });
