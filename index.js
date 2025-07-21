2353eb575d49018392de93d0f52abcbb7c8935cf
Terence Win <terencewin@Terences-MacBook-Air.local>
2025-07-21 23:27:45 +0900

    Sixth time

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/api/shorturl', (req, res) => {
   const shortURL = urlCounter++; 
 
   if (!/^https?:\/\/[a-z]+/.test(originURL)) {
-    return res.status(400).json({error:"Invalid URL"});
+    return res.status(400).json({error: "invalid url"});
   }else{
     let currentURL = {
       "original_url": originURL,
